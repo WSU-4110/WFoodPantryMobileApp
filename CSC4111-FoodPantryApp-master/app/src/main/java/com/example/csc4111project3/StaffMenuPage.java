40bdcfad896f89fef9aca462378db1157370d350
Samuel Pfeiffer <gp3823@wayne.edu>
2021-02-21 14:57:00 -0500

    Commit attempt number

---
+++ CSC4111-FoodPantryApp-master/app/src/main/java/com/example/csc4111project3/StaffMenuPage.java
@@ -0,0 +1,14 @@
+package com.example.csc4111project3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StaffMenuPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staffmenu_page);
+    }
+}
