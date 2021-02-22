6a0fe9aa54cd5b1df9fa1c978a4952fc2284f971
ddfn <gy8494@wayne.edu>
2021-02-22 16:06:03 -0500

    deleting files

--- CSC4111-FoodPantryApp-master/app/src/main/java/com/example/csc4111project3/MenuPage.java
+++ CSC4111-FoodPantryApp-master/app/src/main/java/com/example/csc4111project3/MenuPage.java
@@ -3,25 +3,12 @@ package com.example.csc4111project3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MenuPage extends AppCompatActivity {
-
-    private Button LogOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_page);
-
-        LogOff = (Button) findViewById(R.id.LogOff);
-        LogOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                System.exit(0);
-            }
-        });
     }
 }
