fbc8eabca4b823b6508982fa6809d46575f36ce0
Christopher Borys <fz5243@wayne.edu>
2021-03-01 12:32:06 -0500

    changed class to extend "Fragment"

--- CSC4111-FoodPantryApp-master/app/src/main/java/com/example/csc4111project3/ContactPage.java
+++ CSC4111-FoodPantryApp-master/app/src/main/java/com/example/csc4111project3/ContactPage.java
@@ -1,14 +1,20 @@
 package com.example.csc4111project3;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ContactPage extends AppCompatActivity {
+public class ContactPage extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_page);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_contact_page, container, false);
     }
 }
