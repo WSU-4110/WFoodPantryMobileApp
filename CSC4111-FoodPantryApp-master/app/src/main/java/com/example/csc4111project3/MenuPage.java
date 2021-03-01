c436438f512f4a8a166aa83e5bcae85b4b0d613e
ddfn <gy8494@wayne.edu>
2021-02-28 19:30:16 -0500

    added listener for the contact option in the user menu.

--- CSC4111-FoodPantryApp-master/app/src/main/java/com/example/csc4111project3/MenuPage.java
+++ CSC4111-FoodPantryApp-master/app/src/main/java/com/example/csc4111project3/MenuPage.java
@@ -1,19 +1,26 @@
 package com.example.csc4111project3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.MenuItem;
 import android.widget.Button;
 
-public class MenuPage extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MenuPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView navigationView;
     private DrawerLayout drawer;
+    private MenuItem navContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +30,44 @@ public class MenuPage extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
         drawer = findViewById(R.id.menu_page_drawer_layout);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //this code will handle clicking on the items in the navMenu
+
     }
 
-    @Override
     public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)){
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }
-        else
+        } else
             super.onBackPressed();
     }
 
-}
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_contact) {
+            Intent intent = new Intent(MenuPage.this, ContactPage.class);
+            startActivity(intent);
+            return true;
+
+        } else if (id == R.id.nav_about) {
+            return true;
+        } else if (id == R.id.nav_check_orders) {
+            return true;
+        }
+            return false;
+
+    }
+}//end of MenuPage Class
