f0f3ad9c16cbd7b74b48bcd55fa87f5113e796cb
ddfn <gy8494@wayne.edu>
2021-04-12 21:45:16 -0400

    getter setter methods added to user. test classes established. gradle dependencies updated

--- CSC4111-FoodPantryApp-master/app/src/main/java/com/example/csc4111project3/User.java
+++ CSC4111-FoodPantryApp-master/app/src/main/java/com/example/csc4111project3/User.java
@@ -1,5 +1,11 @@
 package com.example.csc4111project3;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Objects;
+
 public class User {
     public String firstName, lastName, email, phoneNumber;
 
@@ -12,5 +18,67 @@ public class User {
         this.lastName       = lastName;
         this.phoneNumber    = phoneNumber;
         this.email          = email;
+    }
+
+    //getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    //toString and equalTo
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getFirstName(), user.getFirstName()) &&
+                Objects.equals(getLastName(), user.getLastName()) &&
+                Objects.equals(getEmail(), user.getEmail()) &&
+                Objects.equals(getPhoneNumber(), user.getPhoneNumber());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName(), getEmail(), getPhoneNumber());
     }
 }//end of User Class
